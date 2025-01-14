dd19c96830b4f28e141583b018414a71011bb20c
vaporon4a <ilich2009@gmail.com>
2025-01-14 21:05:34 +0700

    fix sql

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,11 +1,20 @@
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+NEW.updated_at = now();
+RETURN NEW;
+END;
+$$ language 'plpgsql';
+
 CREATE TABLE IF NOT EXISTS dates
 (
     uuid               UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     dairy_date              DATE UNIQUE NOT NULL,
-    is_deleted				BOOLEAN NOT NULL DEFAULT 0,
-    created_at				DATETIME NOT NULL DEFAULT NOW(),
-    updated_at				DATETIME NOT NULL ON UPDATE NOW()
+    is_deleted				BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at				TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at				TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE TRIGGER update_modified_time BEFORE UPDATE ON dates FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 
 CREATE TABLE IF NOT EXISTS tasks
 (
@@ -13,10 +22,11 @@ CREATE TABLE IF NOT EXISTS tasks
     dairy_time              TIMESTAMP NOT NULL UNIQUE,
     dairy_task              TEXT  NOT NULL,
     date_uuid               UUID NOT NULL REFERENCES dates(uuid),
-    is_deleted				BOOLEAN NOT NULL DEFAULT 0,
-    created_at				DATETIME NOT NULL DEFAULT NOW(),
-    updated_at				DATETIME NOT NULL ON UPDATE NOW()
+    is_deleted				BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at				TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at				TIMESTAMP NOT NULL DEFAULT NOW()
     
 );
+CREATE TRIGGER update_modified_time BEFORE UPDATE ON tasks FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 CREATE INDEX IF NOT EXISTS idx_time ON tasks (dairy_time);
 CREATE INDEX IF NOT EXISTS idx_task ON tasks (dairy_task);
